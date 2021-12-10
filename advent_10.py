ce5c8b7ec0a59be0486b4ea91e1ab7981d34ae24
Yi Xian <yyx543@users.noreply.github.com>
2021-12-11 00:19:55 +0800

    cleaned up day 10

--- advent_10.py
+++ advent_10.py
@@ -113,36 +113,29 @@ inp_lst = inp_str.split("\n")
 
 count = 0
 scores = []
+open = "([{<"
+close = ")]}>"
+count_lst = [3, 57, 1197, 25137]
+
 for line in inp_lst:
     not_corrupt = True
     temp = ""
     for char in line:
-        if char == "(": temp += ")"
-        elif char == "[": temp += "]"
-        elif char == "{": temp += "}"
-        elif char == "<": temp += ">"
-            
+        if char in open:
+            temp += close[open.index(char)]
         elif char == temp[-1]:
             temp = temp[:-1]
-            
         else:
             not_corrupt = False
-            if char == ")": count += 3
-            elif char == "]": count += 57
-            elif char == "}": count += 1197
-            elif char == ">": count += 25137
+            count += count_lst[close.index(char)]
             break
-        
+
     if not_corrupt and len(temp) != 0:
         curr = 0
         for char in reversed(temp):
-            curr *= 5
-            if char == ")": curr += 1
-            elif char == "]": curr += 2
-            elif char == "}": curr += 3
-            elif char == ">": curr += 4
+            curr = curr*5 + close.index(char)+1
         scores.append(curr)
-        
+
 print(f"Part 1: {count}")
 scores.sort()
 print("Part 2: {}".format(scores[int((len(scores)-1)/2)]))
